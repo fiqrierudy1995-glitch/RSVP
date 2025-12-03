a20d91b3d8f8624c08c303a9a26a52b09bfa6d3d
fiqrierudy1995-glitch <fiqrierudy1995@gmail.com>
2025-12-03 23:49:08 +0800

    Update config.js

--- allen-glow/config.js
+++ allen-glow/config.js
@@ -11,7 +11,7 @@ const weddingDetails = {
   },
   kataAluan: `warmly invite <br>
 Dato’/Datin/Sir/Madam/Mr./Ms.<br>
-to witness and celebrate the love and marriage<br>
+to witness and celebrate<br> the love and marriage<br>
 of our beloved son and daughter.`,
   verse: "With Joy and Gratitude to Almighty God",
   date: {
@@ -31,12 +31,12 @@ of our beloved son and daughter.`,
   programme: [
     { time: "06.00 p.m", activity: "Guest arrival & welcome photo" },
     { time: "07.00 p.m", activity: "Newlyweds' Entrance" },
-    { time: "07.15 p.m", activity: "Grace Dinner served" },
+    { time: "07.15 p.m", activity: "Grace" },
     { time: "08.00 p.m", activity: "Cake Cutting Ceremony" },
     { time: "08.10 p.m", activity: "Speeches" },
     { time: "08.30 p.m", activity: "Moment-Capturing Sessions" },
     { time: "09.00 p.m", activity: "Open Dance Floor" },
-    { time: "10.00 p.m", activity: "Final Blessing & Send-Off" }
+    { time: "10.00 p.m", activity: "End" }
   ],
   prayer: `“Therefore what God has joined together, let no one separate.”<br>— Matthew 19:6`,
   photos: [
@@ -53,6 +53,7 @@ of our beloved son and daughter.`,
     loop: true
   }
 };
+
 
 
 
